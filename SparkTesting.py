e738a88233696c4656e1fefb8d813cc59a597415
pansingla19 <pansingla@deloitte.com>
2019-09-16 20:39:38 +0530

    Adding the comment to read file

--- SparkTesting.py
+++ SparkTesting.py
@@ -3,6 +3,7 @@ from pyspark import SparkConf, SparkContext
 conf = SparkConf().setAppName('SparkTest')
 sc = SparkContext(conf = conf)
 
+#Reading the local text file using SparkContext
 testRDD =sc.textFile(r'file:///C:\Users\pansingla\Downloads\Fiverr Files\kw\test.txt',2)
 
 print(type(testRDD))
