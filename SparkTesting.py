d9c17d683674026faa109ef5486d1b5a3be9cef3
pansingla19 <pansingla@deloitte.com>
2019-09-25 15:23:22 +0530

    Remove New Changes

--- SparkTesting.py
+++ SparkTesting.py
@@ -6,6 +6,6 @@ sc = SparkContext(conf = conf)
 #Reading the local text file using SparkContext
 testRDD =sc.textFile(r'file:///C:\Users\pansingla\Downloads\Fiverr Files\kw\test.txt',2)
 
-print('Adding New Changes')
+print('Removing New Changes')
 print(type(testRDD))
 print(testRDD.collect())
