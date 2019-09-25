9f9fa3b44aa73870a54384077383c17027857f9c
Pankaj Singla <p.singla69@gmail.com>
2019-09-26 02:12:58 +0530

    Changes from personal PC

--- SparkTesting.py
+++ SparkTesting.py
@@ -4,8 +4,7 @@ conf = SparkConf().setAppName('SparkTest')
 sc = SparkContext(conf = conf)
 
 #Reading the local text file using SparkContext
-testRDD =sc.textFile(r'file:///C:\Users\pansingla\Downloads\Fiverr Files\kw\test.txt',2)
+testRDD =sc.textFile(r'file:///D:\Fiverr\kw\demo_file.txt',2)
 
-print('Removing New Changes')
 print(type(testRDD))
 print(testRDD.collect())
